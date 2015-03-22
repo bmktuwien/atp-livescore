93ae40d12447edd765529e283816fb0c4e2ecb4d
BMK <bmktuwien@gmail.com>
2015-03-22 19:18:55 +0100

    Implement desktop noficiations using 'libnotify'

--- src/AtpLiveScore.hs
+++ src/AtpLiveScore.hs
@@ -1,18 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 module AtpLiveScore where
 
+import           Control.Concurrent
 import           Control.Exception
+import           Control.Monad
 import qualified Data.ByteString        as B
 import qualified Data.ByteString.Char8  as B8
 import           Data.Char
 import           Data.Maybe
 import           Data.Time.Clock.POSIX
+import           Libnotify
 import           Network.Http.Client
 import qualified System.IO.Streams      as Streams
 import           Text.HTML.TagSoup
 import           Text.HTML.TagSoup.Tree
+import           Text.Printf
 import           Text.Read
+
+-------------------------------------------------------------------------------
 
 data Player = Player
     { playerName        :: !B.ByteString
@@ -26,6 +33,27 @@ data Score = Score
     , scorePlayer2 :: !Player
     } deriving (Eq, Show)
 
+-------------------------------------------------------------------------------
+startTicker :: IO ()
+startTicker = tickerLoop
+  where
+    tickerLoop = do
+      mResp <- getScore
+
+      case mResp of
+       Nothing   -> return ()
+       Just resp -> do
+         let scores = parseScores resp
+
+         mapM_ notifyScore scores
+
+      -- wait for 5 sec
+      threadDelay 10000000
+
+      tickerLoop
+
+-- | Retrieves the score from 'tennislive.at' server.
+-- The response is an HTML document which should be parsed by 'parseScore'.
 getScore :: IO (Maybe B.ByteString)
 getScore =
   bracket
@@ -41,8 +69,12 @@ getScore =
 
     receiveResponse conn $ \p is -> Streams.read is
 
-parseScore :: B.ByteString -> [Score]
-parseScore inp = extractScores matchTable
+-- | Parses the HTML response from the 'tennislive.at' server.
+-- This function assumes a certain structure of the response, so if the
+-- assumed structure does not match, this function can fail or return
+-- non-sensical data.
+parseScores :: B.ByteString -> [Score]
+parseScores inp = extractScores matchTable
   where
     -- second element in the list is our match table
     matchTable = fromJust . cleanWhiteSpace $
@@ -83,6 +115,8 @@ parseScore inp = extractScores matchTable
 
       Player name [set1, set2, set3, set4, set5] currentGame
 
+-------------------------------------------------------------------------------
+
 cleanWhiteSpace :: TagTree B.ByteString -> Maybe (TagTree B.ByteString)
 cleanWhiteSpace t@(TagLeaf (TagText str))
       | B8.all isSpace str = Nothing
@@ -100,3 +134,27 @@ lookupText (TagBranch _ _ subtrees) =
 
 lookupInt :: TagTree B.ByteString -> Maybe Int
 lookupInt = maybe Nothing (readMaybe . B8.unpack) . lookupText
+
+formatScore :: Score -> String
+formatScore Score{..} =
+  concat [ printf "Match: %s\n" $ B8.unpack scoreMatch
+         , formatPlayer scorePlayer1
+         , formatPlayer scorePlayer2
+         ]
+
+formatPlayer :: Player -> String
+formatPlayer Player{..} =
+  let [s1,s2,s3,s4,s5] = playerSets in
+
+  printf "%d | %d | %d | %d | %d || %.2d   (%s) \n"
+         s1 s2 s3 s4 s5 playerCurrentGame $ B8.unpack playerName
+
+notifyScore :: Score -> IO ()
+notifyScore Score{..} =
+  display_ $
+       summary (printf "%s" $ B8.unpack scoreMatch)
+    <> body (concat [ formatPlayer scorePlayer1
+                    , formatPlayer scorePlayer2
+                    ])
+    <> icon "dialog-information"
+    <> timeout Default
--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,6 @@
 module Main (main) where
 
 import           AtpLiveScore
-import           Control.Applicative
-import           Data.Maybe
 
 main :: IO ()
-main = do
-  resp <- fromJust <$> getScore
-  let scores = parseScore resp
-
-  print scores
+main = startTicker
